54c041cb2afc039537018cee5767a54fc0cec3cb
Никита Лукин <gonka04@gmail.com>
2025-02-23 13:00:32 +0300

    немного порядка и начало авторихации

--- lukoshkino/Program.cs
+++ lukoshkino/Program.cs
@@ -1,4 +1,6 @@
 using lukoshkino.Components;
+using lukoshkino.ViewModels;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/login");
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ lukoshkino/ViewModels/VM_Authorization.cs
@@ -0,0 +1,9 @@
+﻿namespace lukoshkino.ViewModels
+{
+    public class VM_Authorization
+    {
+        public string Login { get; set; } = "";
+
+        public string Password { get; set; } = "";
+    }
+}
